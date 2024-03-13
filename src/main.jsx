3b39c0be431aafab2c64eb846e5461b4118baff2
LAV1E <bundelaanshika1233@gmail.com>
2024-03-13 18:46:51 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ModalTest from './modal-test.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ModalTest/>
+  </React.StrictMode>,
+)
